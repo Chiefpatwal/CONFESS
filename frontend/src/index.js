e823364c9e0569bb5c7102a3c448510b3e8a8433
PANKAJ PATWAL <pankajpatwal1224@gmail.com>
2025-09-02 02:10:57 +0530

    version 1

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 // import App from "./TestDaisyUI"; // Temporary test
 import { ClerkProvider } from "@clerk/clerk-react";
 
-const PUBLISHABLE_KEY = "pk_live_Y2xlcmsuZXhhbXBsZTExMTExLmNvbSQ"; // from Clerk dashboard
+const PUBLISHABLE_KEY = "pk_test_dmFzdC11cmNoaW4tNTcuY2xlcmsuYWNjb3VudHMuZGV2JA"; // from Clerk dashboard
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
